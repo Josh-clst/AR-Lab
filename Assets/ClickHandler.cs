3c611a1817173f70ff66439b9a94077bd3cf1a4a
Josh-clst <joshuaicard57@gmail.com>
2024-11-06 08:58:09 +0100

    Contextual Information and Customization

--- Assets/ClickHandler.cs
+++ Assets/ClickHandler.cs
@@ -1,31 +1,37 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Example : MonoBehaviour, IPointerClickHandler
-{
+{   
+    private int nbClicks = 0;
     private bool isClicked = false;
     //Detect if a click occurs
     public void OnPointerClick(PointerEventData pointerEventData)
     {
         GameObject clickedObject = pointerEventData.pointerCurrentRaycast.gameObject;
-        if (clickedObject != null)
+        GameObject parent = clickedObject.transform.parent.gameObject;
+        if (parent != null)
         {
-            Renderer rend = clickedObject.GetComponent<Renderer>();
-            if (rend != null)
-            {
-                
-                // Change the material color
-                if (!isClicked) 
+            Renderer[] rends = parent.GetComponentsInChildren<Renderer>();
+            TMP_Text text = parent.GetComponentInChildren<TMP_Text>();
+            nbClicks++;
+            text.text = clickedObject.name + "\n" + "Nb of interactions: " + nbClicks;
+            foreach (Renderer rend in rends) {
+                if (rend != null)
                 {
-                    rend.material.color = Color.black;
-                    isClicked = true;
-                }
-                else 
-                {
-                    rend.material.color = Color.yellow;
-                    isClicked = false;
+                    // Change the material color
+                    if (!isClicked) 
+                    {
+                        rend.material.color = Color.black;
+                    }
+                    else 
+                    {
+                        rend.material.color = Color.yellow;
+                    }
                 }
             }
+            isClicked = !isClicked;
         }
     }
 }
